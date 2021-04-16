4be658aecd249b11bd7427983fcd36052080983e
Mark de Graaff <60252744+MarkdeGraaff@users.noreply.github.com>
2021-04-16 18:50:34 +0200

    added test

---
+++ commands/money/check-bal.js
@@ -0,0 +1,13 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'check-bal',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        console.log("test");
+    }
+}
